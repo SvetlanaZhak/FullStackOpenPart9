a2779924c3972257e999b36e7ba04a340fdb4265
SvetlanaZhak <lana.zhak@gmail.com>
2021-02-22 20:54:08 +0200

    add 9.22

--- partC/patientor/src/components/DiagnoseCodeInfo.tsx
+++ partC/patientor/src/components/DiagnoseCodeInfo.tsx
@@ -2,19 +2,35 @@ import React from 'react';
 import { Diagnosis} from '../types';
 import { useStateValue } from "../state";
 
-export const DiagnoseInfo: React.FC <{diagnoseCode: Array<string | undefined>}> = ({diagnoseCode}) =>{
+export const DiagnoseInfo: React.FC <{diagnoseCodes: Array<string | undefined>}> = ({diagnoseCodes}) =>{
     const [{ diagnoses }] = useStateValue();
     const diagnose = Object.values(diagnoses);
     return (
         <div>
-{/* {diagnoseCode ?  <p>{diagnose?.code}{diagnose?.name}   </p>:
+      {diagnoseCodes ?  
+        diagnoseCodes.map(code => (
+            <ul key = {code}>
+ 
+                <li>{diagnose.find(i => i.code === code)?.code}&nbsp;
+                 {diagnose.find(i => i.code === code)?.name}
+                 </li>
+             </ul>
+ ))
+:
+<p>No diagnoses info</p>}
 
-<p>No diagnose info</p>
-} */}
+</div>
+    )}
 
-
+{/* const diagnoseInfo = entry.diagnosisCodes?.map((code) => (
+  <ul key={code}>
     
-        </div>
-      );
-}
-    
+  <li>{diagnose.find(i => i.code === code)?.code}
+  {diagnose.find(i => i.code === code)?.name}  
+  
+  </li>
+   
+   
+  </ul>
+  
+)); */}
--- partC/patientor/src/components/EntryInfo.tsx
+++ partC/patientor/src/components/EntryInfo.tsx
@@ -1,79 +1,86 @@
 import React from 'react';
-import { Entry, HospitalEntry} from '../types';
+import { Entry, HospitalEntry, OccupationalHealthcareEntry, HealthCheckEntry  } from '../types';
 import {DiagnoseInfo} from "./DiagnoseCodeInfo"
 import { useStateValue } from "../state";
 import { Segment, Header, Icon, List } from 'semantic-ui-react';
 
 
 
-const assertNever = (value: never): never => {
-    throw new Error(
-      `Unhandled discriminated union member: ${JSON.stringify(value)}`
-    );
-  };
-
-
-// const HospitalEntry: React.FC<{entry: HospitalEntry}> = ({entry}) => {
-//       if (entry !== undefined) {
-//         return (
-//             <div>
-//                     <Segment>
-//                     <Header as="h5">Discharge</Header>
-//                     <b>Date: {entry?.discharge.date}</b>
-//                     <b>Criteria: {entry?.discharge.criteria}</b>
-//                 </Segment>
-//             </div>
+const HospitalEntries: React.FC<{entry: HospitalEntry}> = ({entry}) => {
+     
+        return (
+            <div>
+                    <Segment>
+                    <Header as="h1">{entry.date} <Icon name ="user md"/></Header>
+                    <p><b>Description:</b> {entry.description}</p> <br/>
+                    <p><b>Specialist: </b>{entry.specialist}</p> <br/>
+                    <DiagnoseInfo diagnoseCodes ={entry.diagnosisCodes || []} />
+                
+                </Segment>
+            </div>
             
             
-//         );
-//       }
+        );
+      
    
-// };
-export const EntryInfo: React.FC <{entry: Entry}> = ({entry}) =>{
+};
+
+
+const OccupationalEntry: React.FC<{entry: OccupationalHealthcareEntry}> = ({entry}) => {
+   
+      return (
+          <div>
+                  <Segment>
+                  <Header as="h1">{entry.date} <Icon name ="stethoscope"/></Header> 
+                  <p><b>Description:</b> {entry.description}</p> <br/>
+                    <p><b>Specialist: </b>{entry.specialist}</p> <br/>
+                    <p><b>Employer: </b>{entry.employerName}</p>
+                  <DiagnoseInfo diagnoseCodes ={entry.diagnosisCodes || []} />
+            
+              </Segment>
+          </div>
+          
+          
+      );
+    
+};
+
+
+
+const HealthCkeckEntry: React.FC<{entry: HealthCheckEntry}> = ({entry}) => {
+
+      return (
+          <div>
+                  <Segment>
+                  <Header as="h1">{entry.date} <Icon name ="user md"/></Header>
+                  <p><b>Description:</b> {entry.description}</p> <br/>
+                    <p><b>Specialist: </b>{entry.specialist}</p> <br/>
+                  
+                  <DiagnoseInfo diagnoseCodes ={entry.diagnosisCodes || []} />
+                  <Icon name ="heart"/>
+              </Segment>
+          </div>
+          
+          
+      );
+    
+};
+export const EntryInfo: React.FC<{entry: Entry}> = ({entry}) =>{
     const [{ diagnoses } ] = useStateValue();
     const diagnose = Object.values(diagnoses);
     console.log(diagnose, "diagnose")
     
-    const diagnoseInfo = entry.diagnosisCodes?.map((code) => (
-        <ul key={code}>
-          
-        <li>{diagnose.find(i => i.code === code)?.code}
-        {diagnose.find(i => i.code === code)?.name}  
-        
-        </li>
-         
-         
-        </ul>
-        
-    ));
-
-
  
-    // switch(entry.type){
-    //     case"Hospital":
-    //         return <HospitalEntry entry= {entry}/>;
-    //     case "OccupationalHealthcare":
-    //         return <OccupationalEntry entry= {entry}/>
-    //     case "HealthCheck":
-    //         return <HealthCkeckEntry entry= {entry}/>
-    //     default: assertNever(entry)
-    // }
-    return (
-        <div>
-        <p>{entry.date} {entry.description}</p>
-        
-        {/* <ul>{  entry.diagnosisCodes?.length !== undefined && entry.diagnosisCodes?.length > 1 ?
-        entry.diagnosisCodes?.map((code, key) => <li key={key}>{code}</li>) 
-
-
-        : <li>{entry.diagnosisCodes}</li>
-        
-        }
-            </ul> */}
-            {diagnoseInfo}
-      
-        </div>
-      );
+    switch(entry.type){
+        case"Hospital":
+            return <HospitalEntries entry={entry}/>;
+        case "OccupationalHealthcare":
+            return <OccupationalEntry entry= {entry}/>
+        case "HealthCheck":
+            return <HealthCkeckEntry entry= {entry}/>
+        default:
+            throw new Error(`Unhandled discriminated union member: ${JSON.stringify(entry)}`);
+    }
 }
     
   
